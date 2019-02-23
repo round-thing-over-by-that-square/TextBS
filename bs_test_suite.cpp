3e6a722d7085fe3b8c526bfdc16ea52f5ff56f84
round-thing-over-by-that-square <lewandowski.alex@gmail.com>
2019-02-23 02:39:19 -0900

    add Ship::getcoords() tests for all directions (N, S, E, W)

--- bs_test_suite.cpp
+++ bs_test_suite.cpp
@@ -15,9 +15,30 @@ TEST_CASE("return the correct ship length", "[s.getLen]") {
 	REQUIRE(s.getLen() == 4);
 }
 
-TEST_CASE("return the correct vector of coords", "[s.getcoords]") {
+TEST_CASE("return the correct vector of coords, North", "[s.getcoords]") {
 	Ship s = Ship(4, make_pair(1, 2), 'N');
 	s.gencoords();
 	std::vector<std::pair<int, int>> v = { make_pair(1,2), make_pair(1,3), make_pair(1,4), make_pair(1,5) };
+	REQUIRE(s.getCoords() == v);
+}
+
+TEST_CASE("return the correct vector of coords, South", "[s.getcoords]") {
+	Ship s = Ship(4, make_pair(1, 10), 'S');
+	s.gencoords();
+	std::vector<std::pair<int, int>> v = { make_pair(1,10), make_pair(1,9), make_pair(1,8), make_pair(1,7) };
+	REQUIRE(s.getCoords() == v);
+}
+
+TEST_CASE("return the correct vector of coords, East", "[s.getcoords]") {
+	Ship s = Ship(4, make_pair(1, 2), 'E');
+	s.gencoords();
+	std::vector<std::pair<int, int>> v = { make_pair(1,2), make_pair(2,2), make_pair(3,2), make_pair(4,2) };
+	REQUIRE(s.getCoords() == v);
+}
+
+TEST_CASE("return the correct vector of coords, West", "[s.getcoords]") {
+	Ship s = Ship(4, make_pair(10, 2), 'W');
+	s.gencoords();
+	std::vector<std::pair<int, int>> v = { make_pair(10,2), make_pair(9,2), make_pair(8,2), make_pair(7,2) };
 	REQUIRE(s.getCoords() == v);
 }
